a677e7baea916fda3078915917ac8528aaae8a05
Andreas Wagner <andreas.wagner@em.uni-frankfurt.de.fake>
2023-06-11 15:29:34 +0200

    Change salamanca namespace to httpS.

--- searchengine/sphinxsearch-config/xmlpipe.sh
+++ searchengine/sphinxsearch-config/xmlpipe.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-find /var/data/existdb/data/export -type f -name "*.snippet.xml" -exec sed -i 's| xmlns:sphinx="http://www.salamanca.school/xquery/sphinx"||g' {} +  >/dev/null 2>&1
+find /var/data/existdb/data/export -type f -name "*.snippet.xml" -exec sed -i 's| xmlns:sphinx="https://www.salamanca.school/xquery/sphinx"||g' {} +  >/dev/null 2>&1
 
 echo '<?xml version="1.0" encoding="utf-8" standalone="yes"?>'
 # curl --insecure --silent https://test.salamanca.school:8443/exist/apps/salamanca/sphinx-client.xql?mode=load
